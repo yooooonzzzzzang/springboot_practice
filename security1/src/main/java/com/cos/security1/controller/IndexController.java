6e5bb7aff7618a965b6005b164a7a450cc7cc3d7
yooooonzzzzzang <cherishvalue@naver.com>
2023-03-06 23:50:26 +0900

    add 시큐리티 환경설정

---
+++ security1/src/main/java/com/cos/security1/controller/IndexController.java
@@ -0,0 +1,43 @@
+package com.cos.security1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller  // view 를 리턴하겠다
+public class IndexController {
+
+    // localhost:8080/
+    // localhost:8080
+    @GetMapping({"","/"})
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody String user(){
+        return "user";
+    }
+    @GetMapping("/admin")
+    public @ResponseBody String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody String manager(){
+        return "manager";
+    }
+    // 스프링 시큐리티가 해당 주소를 낚아챔 -> SecurityConfig 파일 생성후 작동 안함
+    @GetMapping("/login")
+    public @ResponseBody String login(){
+        return "login";
+    }
+    @GetMapping("/join")
+    public @ResponseBody String join(){
+        return "join";
+    }
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc(){
+        return "회원가입 완료됨";
+    }
+}
