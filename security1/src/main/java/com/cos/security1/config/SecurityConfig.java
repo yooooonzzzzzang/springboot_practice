a4749b21420635d935b545a7237041e4243d5d36
yooooonzzzzzang <cherishvalue@naver.com>
2023-03-07 17:57:30 +0900

    add 3강

--- security1/src/main/java/com/cos/security1/config/SecurityConfig.java
+++ security1/src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -1,13 +1,20 @@
 package com.cos.security1.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터(SecurityConfig)가 스프링 필터체인에 등록됨
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    // 패스워드 암호화
+    @Bean // 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다
+    public BCryptPasswordEncoder encodePwd(){
+        return new BCryptPasswordEncoder();
+    }
     @Override
     protected void configure(HttpSecurity http)throws Exception{
         http.csrf().disable();
@@ -18,6 +25,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().permitAll()
                 .and() // 권한이 없는 페이지에 접근하면 로그인 화면으로 이동
                 .formLogin()
-                .loginPage("/login");
+                .loginPage("/loginForm");
     }
 }
--- security1/src/main/java/com/cos/security1/controller/IndexController.java
+++ security1/src/main/java/com/cos/security1/controller/IndexController.java
@@ -1,43 +1,62 @@
 package com.cos.security1.controller;
 
+import com.cos.security1.model.User;
+import com.cos.security1.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller  // view 를 리턴하겠다
 public class IndexController {
+    @Autowired
+    private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
     // localhost:8080/
     // localhost:8080
-    @GetMapping({"","/"})
-    public String index(){
+    @GetMapping({"", "/"})
+    public String index() {
         return "index";
     }
 
     @GetMapping("/user")
-    public @ResponseBody String user(){
+    public @ResponseBody String user() {
         return "user";
     }
+
     @GetMapping("/admin")
-    public @ResponseBody String admin(){
+    public @ResponseBody String admin() {
         return "admin";
     }
 
     @GetMapping("/manager")
-    public @ResponseBody String manager(){
+    public @ResponseBody String manager() {
         return "manager";
     }
+
     // 스프링 시큐리티가 해당 주소를 낚아챔 -> SecurityConfig 파일 생성후 작동 안함
-    @GetMapping("/login")
-    public @ResponseBody String login(){
-        return "login";
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "loginForm";
     }
-    @GetMapping("/join")
-    public @ResponseBody String join(){
-        return "join";
+
+    @GetMapping("/joinForm")
+    public String joinForm() {
+        return "joinForm";
     }
-    @GetMapping("/joinProc")
-    public @ResponseBody String joinProc(){
-        return "회원가입 완료됨";
+
+    @PostMapping("/join")
+    public String join(User user) {
+        user.setRole("ROLE_USER");
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        userRepository.save(user); // 패스워드가 암호화 되지 않았기에 시큐리티로 로그인할 수 없음
+        return "redirect:/loginForm";
     }
 }
