fa3bbc2b19fc24ed49e18394fa8ceb9398337dda
yooooonzzzzzang <cherishvalue@naver.com>
2023-07-30 19:26:38 +0900

    remove .git files

---
+++ w1/src/main/java/com/zerock/w1/MyServlet.java
@@ -0,0 +1,22 @@
+package com.zerock.w1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name ="myServlet", urlPatterns = "/my") // 브라우저의 경로와 해당 서블릿을 연결
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        PrintWriter out = resp.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>MyServlet</h1>");
+        out.println("</body></html>");
+    }
+}
