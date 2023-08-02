775a9886b9e6faaf29e061ac18e21a2403d21161
yooooonzzzzzang <cherishvalue@naver.com>
2023-08-02 17:27:43 +0900

    add InputController

---
+++ w1/src/main/java/com/zerock/w1/calc/InputController.java
@@ -0,0 +1,17 @@
+package com.zerock.w1.calc;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "inputController", urlPatterns = "/cal/input")
+public class InputController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("InputController...doGet...");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/calc/input.jsp");
+        dispatcher.forward(request,response);
+    }
+
+}
