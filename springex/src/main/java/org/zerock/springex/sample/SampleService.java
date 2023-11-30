d345ecd6772669cc519ac828c33237e045dacdd3
yooooonzzzzzang <cherishvalue@naver.com>
2023-11-30 22:45:59 +0900

    modify: sampleService 생성자 주입

--- springex/src/main/java/org/zerock/springex/sample/SampleService.java
+++ springex/src/main/java/org/zerock/springex/sample/SampleService.java
@@ -1,4 +1,14 @@
 package org.zerock.springex.sample;
 
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@ToString
+@RequiredArgsConstructor // 생성자 주입, + final
 public class SampleService {
+
+    private final SampleDAO sampleDAO;
 }
