cb99ec7a4880e149440a38aaf55d7f8c2bcf633c
yooooonzzzzzang <cherishvalue@naver.com>
2023-12-11 22:54:29 +0900

    add selectAll

--- Todo/src/main/java/org/zerock/springex/mapper/TodoMapper.java
+++ Todo/src/main/java/org/zerock/springex/mapper/TodoMapper.java
@@ -2,8 +2,12 @@ package org.zerock.springex.mapper;
 
 import org.zerock.springex.domain.TodoVO;
 
+import java.util.List;
+
 public interface TodoMapper {
     String getTime();
 
     void insert(TodoVO todoVO);
+
+    List<TodoVO> selectAll();
 }
