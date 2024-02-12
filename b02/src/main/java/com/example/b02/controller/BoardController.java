d6e83bf4aba4927136ee6e6194528d1057a8347e
yooooonzzzzzang <cherishvalue@naver.com>
2024-02-12 19:31:03 +0900

    add Simple Sidebar

---
+++ b02/src/main/java/com/example/b02/controller/BoardController.java
@@ -0,0 +1,23 @@
+package com.example.b02.controller;
+
+import com.example.b02.dto.PageRequestDTO;
+import com.example.b02.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+@Log4j2
+@RequiredArgsConstructor
+public class BoardController {
+    private final BoardService boardService;
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+
+    }
+}
