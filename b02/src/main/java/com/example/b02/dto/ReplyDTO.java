16cf90e76f5c2064da6e8bd9268d7be9083f385a
yooooonzzzzzang <cherishvalue@naver.com>
2024-02-27 11:44:41 +0900

    add ReplyController

---
+++ b02/src/main/java/com/example/b02/dto/ReplyDTO.java
@@ -0,0 +1,20 @@
+package com.example.b02.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReplyDTO {
+    private Long rno;
+    private Long bno;
+    private String replyText;
+    private String replyer;
+    private LocalDateTime regDate, modDate;
+}
